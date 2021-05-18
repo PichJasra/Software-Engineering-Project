8bc2339c113e2b50febcc00671d0be0d44b8ba8a
PichJasra <43584579+PichJasra@users.noreply.github.com>
2021-05-18 14:01:56 +0200

    Final update. Didn't have time for javadoc

--- SEProject/src/main/java/gui/package-info.java
+++ SEProject/src/main/java/gui/package-info.java
@@ -1,3 +1,8 @@
-package gui;
+/**
+ * This package contains the graphical user interface implemented for the game.
+ * I made two scenes, one is the menu, the other one is where the game will take place.
+ * The various informations will come up as an alert box or confirmation box, depending on the situation.
+ * @author Ferenc Bodnár
+ */
 
-//This package contains classes which represent the menu, the alert boxes, as well as the main class
+package gui;
