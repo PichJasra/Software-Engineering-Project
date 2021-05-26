5560dcc4204e1eff5c60f3ffef9844461fcc0810
PichJasra <43584579+PichJasra@users.noreply.github.com>
2021-05-26 21:50:58 +0200

    Add files via upload

--- SEProject/test/game/GameControllerTest.java
+++ SEProject/test/game/GameControllerTest.java
@@ -1,37 +1,8 @@
 package game;
 
-import board.ChessboardModel;
-import board.ChessboardView;
-import board.FieldModel;
 import org.junit.jupiter.api.Test;
-import pieces.PieceFactory;
-import pieces.PieceModel;
-import pieces.PieceView;
-
-import java.util.ArrayList;
 
 class GameControllerTest {
-
-    @Test
-    void undoLastMove() {
-        ChessboardModel chessboardModel = new ChessboardModel();
-        ChessboardView chessboardView = new ChessboardView(chessboardModel);
-        PieceFactory pieceFactory = new PieceFactory(chessboardModel, chessboardView);
-        GameController gameController = new GameController(chessboardModel, chessboardView,
-                pieceFactory.placePieces(true), true);
-        assert !chessboardModel.canUndo();
-        FieldModel foxField = chessboardModel.getField(0, 5);
-        FieldModel toMove = chessboardModel.getField(1, 4);
-        PieceModel fox = foxField.getPieceModel();
-        PieceView foxView = chessboardView.getFieldView(foxField).getPieceView();
-        foxView.move(toMove);
-        assert !fox.hasTurnNow();
-        assert fox == chessboardModel.getField(1, 4).getPieceModel();
-        assert null == chessboardModel.getField(0, 5).getPieceModel();
-        gameController.undoLastMove();
-        assert fox == chessboardModel.getField(0, 5).getPieceModel();
-        assert fox.hasTurnNow();
-    }
 
     @Test
     void getHound() {
